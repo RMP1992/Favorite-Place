1fe6d291e396dc6cbd536db451290265a94dd11a
RMP1992 <45944114+RMP1992@users.noreply.github.com>
2020-04-25 01:37:09 +0100

    finished react side

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
 
 import Users from './user/pages/users';
-import NewPlace from './places/pages/newPlace';
+import NewPlace from './places/pages/NewPlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './user/pages/Auth';
+import { AuthContext } from './shared/context/auth-context';
 import './App.css';
 
 const App = () => {
-  return <Router>
-    <MainNavigation/>
-    <main>
-    <Switch>
-      <Route path= "/" exact component={Users}/>
-      <Route path= "/:userId/places" exact component= {UserPlaces}/>
-      <Route path= "/places/new" exact component= {NewPlace}/>
-      <Redirect to="/" />
-    </Switch>
-    </main>
-  </Router>
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true)
+  }, [])
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false)
+  }, [])
+
+  let routes;
+
+  if(isLoggedIn) {
+    routes = (
+      <Switch>
+        <Route path= "/" exact component={Users}/>
+        <Route path= "/:userId/places" exact component= {UserPlaces}/>
+        <Route path= "/places/new" exact component= {NewPlace}/>
+        <Route path= "/:userId/places/:placeId" exact component= {UpdatePlace}/>
+
+        <Redirect to="/" />
+      </Switch>
+    );
+  }else{
+    routes = (
+      <Switch>
+        <Route path= "/" exact component={Users}/>
+        <Route path= "/:userId/places" exact component= {UserPlaces}/>
+        <Route path= "/auth" exact component= {Auth}/>
+        <Redirect to="/auth" />
+      </Switch>
+    )
+  }
+
+  return (
+    <AuthContext.Provider 
+      value={{isLoggedIn: isLoggedIn, login: login, logout: logout}}>
+      <Router>
+        <MainNavigation/>
+        <main>
+          
+            {routes}
+          
+        </main>
+      </Router>
+    </AuthContext.Provider>
+  )
 
 }
 
--- src/places/components/PlaceItem.jsx
+++ src/places/components/PlaceItem.jsx
@@ -1,17 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 
 import Card from '../../shared/components/UIelements/Card';
 import Button from '../../shared/components/FormElements/Button';
 import Modal from '../../shared/components/UIelements/Modal';
 import Map from '../../shared/components/UIelements/Map';
+import { AuthContext } from '../../shared/context/auth-context';
 import './PlaceItem.css';
 
 const PlaceItem = (props) =>{
+    const auth = useContext(AuthContext)
+
     const [showMap, setShowMap] = useState(false);
+    const [showConfirmModal, setShowConfirmModal] = useState(false)
 
     const openMapHandler = () => setShowMap(true);
 
     const closeMapHandler = () => setShowMap(false);
+    const showDeleteWarningHandler = () =>{
+        setShowConfirmModal(true)
+    }
+    const cancelDeleteHandler = () =>{
+        setShowConfirmModal(false)
+    }
+    const confirmDeleteHandler = () =>{
+        setShowConfirmModal(false)
+        console.log('Deleting...')
+    }
+
     return(
         <React.Fragment>
             <Modal 
@@ -26,6 +41,20 @@ const PlaceItem = (props) =>{
                     <Map center={props.coordinates} zoom={16} />
                 </div>
             </Modal>
+            <Modal 
+                show={showConfirmModal}
+                onCancel={cancelDeleteHandler}
+                header="Are you sure?" 
+                footerClass="place-item__modal-actions"
+                footer={
+                    <React.Fragment>
+                        <Button inverse onClick={cancelDeleteHandler} >Cancel</Button>
+                        <Button danger onClick={confirmDeleteHandler} >Delete</Button>
+                    </React.Fragment>
+                }
+                >
+                <p>Do you want to proceed and delete this place?</p>
+            </Modal>
         <li className='place-item'>
             <Card className='place-item__content'>
             <div className='place-item__image'>
@@ -38,8 +67,8 @@ const PlaceItem = (props) =>{
             </div>
             <div className="place-item__actions">
                 <Button inverse onClick={openMapHandler} >View On Map</Button>
-                <Button to={`places/${props.id}`}>Edit</Button>
-                <Button danger>Delete</Button>
+                {auth.isLoggedIn && <Button to={`places/${props.id}`}>Edit</Button> }
+                {auth.isLoggedIn && <Button danger onClick={showDeleteWarningHandler}>Delete</Button>}
             </div>
             </Card>
         </li>
